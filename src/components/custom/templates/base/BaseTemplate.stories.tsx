49033d92e229d883600b7e23d1353ac6a335efab
Abanoub <naguib.abanoub@gmail.com>
2025-07-31 15:05:40 +0300

    feat: change the stories names and categories for better organization

--- src/components/custom/templates/base/BaseTemplate.stories.tsx
+++ src/components/custom/templates/base/BaseTemplate.stories.tsx
@@ -5,7 +5,7 @@ import { mockBaseTemplateProps } from "./BaseTemplate.mocks";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/BaseTemplate",
+  title: "Templates/ Base Template",
   component: BaseTemplate,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
