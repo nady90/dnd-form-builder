50dc5afb11aa55c3a2317e6842178b6e209da0b3
Abanoub <naguib.abanoub@gmail.com>
2025-07-31 11:31:49 +0300

    build: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from "@eslint/eslintrc";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
@@ -10,15 +13,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: { "simple-import-sort": simpleImportSort },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript"), {
+  plugins: { "simple-import-sort": simpleImportSort },
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
-];
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
