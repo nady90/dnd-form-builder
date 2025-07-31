9d2c6e28947ace101254612013409c22a917e6a5
Abanoub <naguib.abanoub@gmail.com>
2025-07-31 15:00:18 +0300

    fix: use the mock props in the stories directly instead of a hard-coded value

--- src/components/custom/templates/base/BaseTemplate.stories.tsx
+++ src/components/custom/templates/base/BaseTemplate.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 
 import BaseTemplate from "./BaseTemplate";
+import { mockBaseTemplateProps } from "./BaseTemplate.mocks";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -21,6 +22,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    sampleTextProp: "hello",
+    ...mockBaseTemplateProps.base,
   },
 };
