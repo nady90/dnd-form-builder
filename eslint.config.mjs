aafd1f5644d95a20a6513c2500de687a7a6026f9
Abanoub <naguib.abanoub@gmail.com>
2025-08-04 14:12:23 +0300

    chore: fix eslint config file import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import { FlatCompat } from "@eslint/eslintrc";
-
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import storybook from "eslint-plugin-storybook";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
