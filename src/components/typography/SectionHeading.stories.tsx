34bb9fc25cc5b06bbcc7abef61f208726388a783
Abanoub <naguib.abanoub@gmail.com>
2025-08-06 13:48:02 +0300

    chore: add the typography folder to the stories organization

---
+++ src/components/typography/SectionHeading.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import SectionHeading from "./SectionHeading";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Typography/ Main Title",
+  component: SectionHeading,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof SectionHeading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    children: "Create form",
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div className="p-10">
+          <Story />
+        </div>
+      );
+    },
+  ],
+  // play: async ({ canvas }) => {},
+};
