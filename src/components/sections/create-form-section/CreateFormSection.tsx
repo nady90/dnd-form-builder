f2370c9bf4d8670981742f5b9aa3ce577f360335
Abanoub <naguib.abanoub@gmail.com>
2025-08-08 20:13:55 +0300

    chore: finish the create form section with the right action cards

--- src/components/sections/create-form-section/CreateFormSection.tsx
+++ src/components/sections/create-form-section/CreateFormSection.tsx
@@ -22,19 +22,19 @@ const BaseTemplate: React.FC<IBaseTemplate> = ({ title }) => {
               variant="add"
             />
             <ActionCard
-              text="Start From Scratch"
-              subText="Jump right in and build something beautiful"
-              variant="add"
+              text="Use Template"
+              subText="Use a template to create and send a survey faster"
+              variant="template"
             />
             <ActionCard
-              text="Start From Scratch"
-              subText="Jump right in and build something beautiful"
-              variant="add"
+              text="Import Form"
+              subText="Convert your existing forms instantly"
+              variant="import"
             />
             <ActionCard
-              text="Start From Scratch"
-              subText="Jump right in and build something beautiful"
-              variant="add"
+              text="Create with AI"
+              subText="Save time and create forms faster. Let AI hand the first draft"
+              variant="ai"
             />
           </Grid4to1>
         </div>
