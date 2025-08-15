3d4c7dcf9da8f03f8f1b9ec2d8efc78dbbc0e0c3
sayedallaa23 <sayedallaa@icloud.com>
2025-08-15 18:50:31 +0300

    feat: add question type button

---
+++ src/components/icons/MultiLine.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MultiLine() {
+  return (
+    <svg
+      width="15"
+      height="13"
+      viewBox="0 0 15 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.125 10.6973L4.23926 9.58301L4.85449 10.1982L2.6875 12.3721L0.520508 10.1982L1.13574 9.58301L2.25 10.6973V0.375H3.125V10.6973ZM5.75 8.25V7.375H9.25V8.25H5.75ZM5.75 5.625V4.75H11.875V5.625H5.75ZM5.75 2.125H14.5V3H5.75V2.125Z"
+        fill="#1E2939"
+      />
+    </svg>
+  );
+}
+
+export default MultiLine;
---
+++ src/components/icons/Text.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Text() {
+  return (
+    <svg
+      width="14"
+      height="13"
+      viewBox="0 0 14 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_5000_520)">
+        <path
+          d="M2.9165 1.99992C2.9165 1.6905 3.03942 1.39375 3.25821 1.17496C3.47701 0.956168 3.77375 0.833252 4.08317 0.833252H9.9165C10.2259 0.833252 10.5227 0.956168 10.7415 1.17496C10.9603 1.39375 11.0832 1.6905 11.0832 1.99992V2.58325C11.0832 2.73796 11.0217 2.88633 10.9123 2.99573C10.8029 3.10513 10.6545 3.16659 10.4998 3.16659C10.3451 3.16659 10.1968 3.10513 10.0874 2.99573C9.97796 2.88633 9.9165 2.73796 9.9165 2.58325V1.99992H7.58317V8.99992H8.1665C8.32121 8.99992 8.46959 9.06138 8.57898 9.17077C8.68838 9.28017 8.74984 9.42854 8.74984 9.58325C8.74984 9.73796 8.68838 9.88633 8.57898 9.99573C8.46959 10.1051 8.32121 10.1666 8.1665 10.1666H5.83317C5.67846 10.1666 5.53009 10.1051 5.42069 9.99573C5.3113 9.88633 5.24984 9.73796 5.24984 9.58325C5.24984 9.42854 5.3113 9.28017 5.42069 9.17077C5.53009 9.06138 5.67846 8.99992 5.83317 8.99992H6.4165V1.99992H4.08317V2.58325C4.08317 2.73796 4.02171 2.88633 3.91232 2.99573C3.80292 3.10513 3.65455 3.16659 3.49984 3.16659C3.34513 3.16659 3.19675 3.10513 3.08736 2.99573C2.97796 2.88633 2.9165 2.73796 2.9165 2.58325V1.99992Z"
+          fill="black"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_5000_520"
+          x="-1.0835"
+          y="0.833252"
+          width="16.1665"
+          height="17.3333"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_5000_520"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_5000_520"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
+export default Text;
