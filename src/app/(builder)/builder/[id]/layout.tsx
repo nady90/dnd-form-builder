b0b1a87ea3f8284bb3149f7a8950104c4b9cfe9f
sayedallaa23 <sayedallaa@icloud.com>
2025-08-19 22:43:13 +0300

    chore: use form title and description in builder nav bar

--- src/app/(builder)/builder/[id]/layout.tsx
+++ src/app/(builder)/builder/[id]/layout.tsx
@@ -1,16 +1,27 @@
 import React, { ReactNode } from "react";
 
+import { GetFormByIdAction } from "@/actions/form";
 import BuilderBottomNavbar from "@/components/custom/navbars/builder-bottom-navbar/BuilderBottomNavbar";
 import BuilderTopNavbar from "@/components/custom/navbars/builder-top-navbar/BuilderTopNavbar";
 
-export default function BuilderLayout({ children }: { children: ReactNode }) {
+export default async function BuilderLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+  const form = await GetFormByIdAction(Number(id));
   return (
     <div className="flex h-screen flex-col">
       <div>
         <BuilderTopNavbar
-          title={"Form Builder"}
-          description={"Add and customize forms for your needs"}
-          savedAt={new Date()}
+          title={form?.name || "Form Builder"}
+          description={
+            form?.description || "Add and customize forms for your needs"
+          }
+          savedAt={form?.createdAt || new Date()}
         />
         <BuilderBottomNavbar
           link={
