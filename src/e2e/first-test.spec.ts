b2c967f05c5b75c84b1a78f216cfa6dcae14ac8d
nady90 <naguib.abanoub@gmail.com>
2025-09-02 23:14:35 +0300

    test: Take a screen shot and save it

--- src/e2e/first-test.spec.ts
+++ src/e2e/first-test.spec.ts
@@ -5,6 +5,7 @@ test("sign up", async ({ page }) => {
   await setupClerkTestingToken({ page });
 
   await page.goto("http://localhost:3000/dashboard");
+  await page.screenshot({ path: "debug-dashboard.png", fullPage: true });
   const createFormHeading = page.getByText(/all forms/i);
   expect(createFormHeading).toBeVisible();
 });
