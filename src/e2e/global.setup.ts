882454ac2c7f784a7b11bfb6f00d28cc9d1e600c
Abanoub <naguib.abanoub@gmail.com>
2025-09-03 01:22:17 +0300

    test: mainly change the localhost to http localhost and some minor debugging adjustments

--- src/e2e/global.setup.ts
+++ src/e2e/global.setup.ts
@@ -1,15 +1,25 @@
 import { clerk, clerkSetup } from "@clerk/testing/playwright";
 import { test as setup } from "@playwright/test";
+import fs from "fs";
 import path from "path";
 
 // Setup must be run serially, this is necessary if Playwright is configured to run fully parallel: https://playwright.dev/docs/test-parallel
 setup.describe.configure({ mode: "serial" });
 
-const authFile = path.join(__dirname, "../e2e/.clerk/user.json");
+const authFile = path.join(__dirname, ".clerk", "user.json");
 
 setup("global setup", async ({ page }) => {
   await clerkSetup();
-  await page.goto("localhost:3000");
+
+  fs.mkdirSync(path.dirname(authFile), { recursive: true });
+
+  await page.goto("http://localhost:3000", { waitUntil: "networkidle" });
+
+  page.on("console", (msg) => console.log("PAGE LOG: ", msg.text()));
+  page.on("requestfailed", (err) =>
+    console.log("REQUEST FAILED: ", err.url, err.failure()),
+  );
+
   await clerk.signIn({
     page,
     signInParams: {
@@ -20,5 +30,9 @@ setup("global setup", async ({ page }) => {
       password: process.env.E2E_CLERK_USER_PASSWORD!,
     },
   });
+
+  await page.waitForLoadState("networkidle");
+  console.log("After SignIn page URL: ", page.url());
+
   await page.context().storageState({ path: authFile });
 });
