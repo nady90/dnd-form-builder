53ede26205c15687ccb0689d88ca1d176338e785
Abanoub <naguib.abanoub@gmail.com>
2025-09-03 15:09:55 +0300

    test(dnd): add the description for all the test cases needed for the dnd
    This is just the description. The implementation is not finished

--- src/e2e/first-test.spec.ts
+++ src/e2e/first-test.spec.ts
@@ -87,7 +87,7 @@ test.describe("Dashboard Tests", () => {
   });
 });
 
-test.describe("Dnd functionality", () => {
+test.describe("Dnd basic functionality", () => {
   test("Drag a simple textfield on the canvas", async ({ page }) => {
     await setupClerkTestingToken({ page });
 
@@ -397,4 +397,94 @@ test.describe("Dnd functionality", () => {
 
     await expect(page.getByText(/required/i)).not.toBeVisible();
   });
+});
+
+test.describe("dnd complex cases", () => {
+  test("Drag two textfields on the canvas", async ({ page }) => {
+    await setupClerkTestingToken({ page });
+
+    await page.goto("http://localhost:3000/dashboard");
+
+    const createFormBtn = await page.getByText(/start from scratch/i);
+    await expect(createFormBtn).toBeVisible();
+    await createFormBtn.click();
+
+    const nameInput = await page.getByLabel("name");
+    await expect(nameInput).toBeVisible();
+
+    const descriptionInput = await page.getByLabel("description");
+    await expect(descriptionInput).toBeVisible();
+
+    const newFormName = "new form title" + Date.now();
+    await nameInput.fill(newFormName);
+
+    const newFormDescription = "description" + Date.now();
+    await descriptionInput.fill(newFormDescription);
+
+    const saveBtn = await page.getByRole("button", { name: "save" });
+    await expect(saveBtn).toBeVisible();
+
+    await saveBtn.click();
+
+    await page.waitForLoadState("networkidle");
+
+    await page.reload();
+
+    await expect(page.getByText(newFormName)).toBeVisible();
+    await expect(page.getByText(newFormDescription)).toBeVisible();
+
+    const finishFormLinks = await page.getByText(/finish the form/i).all();
+    const lastLink = finishFormLinks[finishFormLinks.length - 1];
+    await lastLink.click();
+    await page.waitForLoadState("networkidle");
+
+    const dragInstruction = page.getByText(
+      /Drag elements from the sidebar to add/i,
+    );
+    await expect(dragInstruction).toBeVisible();
+
+    const textField = page.getByRole("button", { name: /textfield/i });
+    await expect(textField).toBeVisible();
+
+    const builder = page.getByTestId("builder");
+    await expect(builder).toBeVisible();
+
+    await textField.waitFor({ state: "visible" });
+    await builder.waitFor({ state: "visible" });
+
+    await textField.hover();
+    await page.mouse.down();
+
+    // You need to move the mouse to a random location to "cancel" the mouse down
+    await page.mouse.move(1, 1);
+
+    await builder.hover();
+    await page.mouse.up();
+
+    await expect(dragInstruction).not.toBeVisible();
+
+    await textField.hover();
+    await page.mouse.down();
+
+    // You need to move the mouse to a random location to "cancel" the mouse down
+    await page.mouse.move(1, 1);
+
+    await builder.hover();
+    await page.mouse.up();
+
+    const textFieldLabel = await page.getByText(/label here/i).all();
+    await expect(textFieldLabel.length).toBe(2);
+  });
+
+  test("Edit a textfield", () => {});
+
+  test("Edit a textfield then add another field on the builder", () => {});
+
+  test("Edit a textfield then add another field on the builder then change the order", () => {});
+
+  test("Edit a textfield then add another field on the first textfield to the top of it", () => {});
+
+  test("Edit a textfield then add another field on the first textfield to the bottom of it", () => {});
+
+  test("Edit a textfield then add another field on the first textfield to the bottom of it then edit it then add one in between", () => {});
 });
