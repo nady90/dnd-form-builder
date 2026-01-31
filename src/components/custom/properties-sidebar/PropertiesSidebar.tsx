75fafef92257c1665b05f32bd52b20d9ad60f68d
Abanoub <naguib.abanoub@gmail.com>
2026-01-31 20:30:19 +0200

    feat(builder): add exit animations to the ai input

--- src/components/custom/properties-sidebar/PropertiesSidebar.tsx
+++ src/components/custom/properties-sidebar/PropertiesSidebar.tsx
@@ -48,48 +48,64 @@ export default function PropertiesSidebar() {
               <AiButton className="fixed right-3.5 bottom-8 z-50" />
             </span>
           </Dialog.Trigger>
-          <Dialog.Overlay className="fixed inset-0 flex bg-black/10 backdrop-blur-sm" />
-          <Dialog.DialogContent className="ai-input-shadows fixed left-1/2 -translate-x-1/2">
-            <Dialog.DialogTitle className="hidden">
-              Ask AI to generate a form
-            </Dialog.DialogTitle>
-            <div className="relative h-[45px] w-[500px] rounded-[20px]">
-              <motion.form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  setIsModalOpen(false);
-                }}
-                initial={{
-                  scale: 2,
-                  y: -100,
-                }}
-                animate={{
-                  scale: 2,
-                  y: 0,
-                }}
-                className="absolute z-20 h-full w-full rounded-[20px]"
-              >
-                <AiInputIcon className="pointer-events-none absolute top-1/2 left-4 z-10 h-6 w-6 -translate-y-1/2" />
-                <input
-                  id="search-ai"
-                  className="absolute h-full w-full rounded-[20px] bg-black px-12 text-white placeholder:text-white focus:outline-0"
-                  placeholder="Type what you need"
-                />
-                <button type="submit"></button>
-              </motion.form>
-              <motion.div
-                initial={{
-                  scale: 2,
-                  y: -100,
-                }}
-                animate={{
-                  scale: 2,
-                  y: 0,
-                }}
-                className="absolute top-1/2 left-1/2 z-10 h-[53px] w-[510px] -translate-x-1/2 -translate-y-1/2 rounded-[22px] bg-linear-60 from-[#BEDBFF] to-[#2B7FFF]"
-              ></motion.div>
-            </div>
-          </Dialog.DialogContent>
+          <AnimatePresence>
+            {isModalOpen && (
+              <Dialog.Portal forceMount>
+                <Dialog.Overlay className="fixed inset-0 flex bg-black/10 backdrop-blur-sm" />
+                <Dialog.DialogContent className="ai-input-shadows fixed top-[20%] left-1/2 -translate-x-1/2">
+                  <Dialog.DialogTitle className="hidden">
+                    Ask AI to generate a form
+                  </Dialog.DialogTitle>
+                  <div className="relative h-[45px] w-[500px] rounded-[20px]">
+                    <motion.form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        setIsModalOpen(false);
+                      }}
+                      initial={{
+                        y: -100,
+                      }}
+                      animate={{
+                        y: 0,
+                      }}
+                      exit={{
+                        y: -200,
+                        opacity: 0,
+                        transition: {
+                          duration: 0.2,
+                        },
+                      }}
+                      className="absolute z-20 h-full w-full rounded-[20px] bg-emerald-500"
+                    >
+                      <AiInputIcon className="pointer-events-none absolute top-1/2 left-4 z-10 h-6 w-6 -translate-y-1/2" />
+                      <input
+                        id="search-ai"
+                        className="absolute h-full w-full rounded-[20px] bg-black px-12 text-white placeholder:text-white focus:outline-0"
+                        placeholder="Type what you need"
+                      />
+                      <button type="submit"></button>
+                    </motion.form>
+                    <motion.div
+                      initial={{
+                        y: -100,
+                      }}
+                      animate={{
+                        y: 0,
+                      }}
+                      exit={{
+                        y: -200,
+                        opacity: 0,
+                        transition: {
+                          duration: 0.2,
+                        },
+                      }}
+                      className="absolute top-1/2 left-1/2 z-10 h-[53px] w-[510px] -translate-x-1/2 -translate-y-1/2 rounded-[22px] bg-linear-60 from-[#BEDBFF] to-[#2B7FFF]"
+                    ></motion.div>
+                  </div>
+                </Dialog.DialogContent>
+              </Dialog.Portal>
+            )}
+          </AnimatePresence>
         </Dialog.Root>
       </AnimatePresence>
     </div>
